16e6cefd547d8b683135d17fa6d0e9369cfbc1cf
Mary Rose Cook <mary@maryrosecook.com>
2017-11-13 11:55:22 +0000

    Group tests

--- spec/student_quiz_spec.rb
+++ spec/student_quiz_spec.rb
@@ -1,21 +1,23 @@
 require "student_quiz"
 
 describe StudentQuiz do
-    subject { StudentQuiz.new(github_username: "maryrosecook",
-                              chapter_number: "2",
-                              quiz_number: "1") }
   describe "::new" do
+    describe "data availability" do
+      subject { StudentQuiz.new(github_username: "maryrosecook",
+                                chapter_number: "2",
+                                quiz_number: "1") }
 
-    it "exposes github_username" do
-      expect(subject.github_username).to eq("maryrosecook")
-    end
+      it "exposes github_username" do
+        expect(subject.github_username).to eq("maryrosecook")
+      end
 
-    it "exposes chapter_number" do
-      expect(subject.chapter_number).to eq("2")
-    end
+      it "exposes chapter_number" do
+        expect(subject.chapter_number).to eq("2")
+      end
 
-    it "exposes quiz_number" do
-      expect(subject.quiz_number).to eq("1")
+      it "exposes quiz_number" do
+        expect(subject.quiz_number).to eq("1")
+      end
     end
   end
 end
